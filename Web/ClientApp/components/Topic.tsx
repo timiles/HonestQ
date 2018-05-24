fdd8c6ad6168f10240c6be1223da4c22d2a2570e
Tim Iles <tim@timiles.com>
2018-05-24 10:55:46 +0100

    Submit Statement form

--- Web/ClientApp/components/Topic.tsx
+++ Web/ClientApp/components/Topic.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
+import { PostStatementFormModel } from '../server-models';
 import { ApplicationState } from '../store';
 import * as TopicStore from '../store/Topic';
+import SubmitButton from './shared/SubmitButton';
 
 type TopicProps = TopicStore.TopicState
     & typeof TopicStore.actionCreators
     & RouteComponentProps<{ topicUrlFragment: string }>;
 
-class Topic extends React.Component<TopicProps> {
+class Topic extends React.Component<TopicProps, PostStatementFormModel> {
+
+    constructor(props: TopicProps) {
+        super(props);
+
+        this.state = { text: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
     public componentWillMount() {
         if (this.props.urlFragment !== this.props.match.params.topicUrlFragment) {
@@ -16,17 +27,58 @@ class Topic extends React.Component<TopicProps> {
         }
     }
 
+    public componentWillReceiveProps(nextProps: TopicProps) {
+        // This will reset the form when a statement has been successfully submitted
+        if (!nextProps.submittedStatement) {
+            this.setState({ text: '' });
+        }
+    }
+
     public render() {
-        const { loading, error, topic } = this.props;
+        const { loading, error, topic, urlFragment, submittingStatement, submittedStatement } = this.props;
+        const { text } = this.state;
         return (
             <div>
                 {loading && <p>Loading...</p>}
                 {error && <div className="alert alert-danger" role="alert">{error}</div>}
                 {topic && (
-                    <h1>{topic.name}</h1>
+                    <div>
+                        <h1>{topic.name}</h1>
+                        <div className="col-md-6">
+                            <h2>Submit New Statement</h2>
+                            {error && <div className="alert alert-danger" role="alert">{error}</div>}
+                            <form name="form" onSubmit={this.handleSubmit}>
+                                <div className={'form-group' + (submittedStatement && !text ? ' has-error' : '')}>
+                                    <label htmlFor="text">Statement</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="text"
+                                        value={text}
+                                        onChange={this.handleChange}
+                                    />
+                                    {submittedStatement && !text && <div className="help-block">Text is required</div>}
+                                </div>
+                                <div className="form-group">
+                                    <SubmitButton submitting={submittingStatement} />
+                                </div>
+                            </form>
+                            {topic.statements.map((x, i) => <h2 key={`statement_${i}`}>&ldquo;{x.text}&rdquo;</h2>)}
+                        </div>
+                    </div>
                 )}
             </div>
         );
+    }
+
+    private handleChange(event: React.FormEvent<HTMLInputElement>): void {
+        const { name, value } = event.currentTarget;
+        this.setState({ ...this.state, [name]: value });
+    }
+
+    private handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        this.props.submit(this.props.match.params.topicUrlFragment, this.state);
     }
 }
 
--- Web/ClientApp/store/Topic.ts
+++ Web/ClientApp/store/Topic.ts
@@ -1,6 +1,6 @@
 ﻿import { addTask, fetch } from 'domain-task';
 import { Reducer } from 'redux';
-import { GetTopicModel } from '../server-models';
+import { GetTopicModel, PostStatementFormModel, StatementListItemModel } from '../server-models';
 import * as Utils from '../utils';
 import { AppThunkAction } from './';
 
@@ -11,7 +11,9 @@ export interface TopicState {
     loading: boolean;
     urlFragment?: string;
     topic?: GetTopicModel;
-    error?: string;
+    submittingStatement?: boolean;
+    submittedStatement?: boolean;
+    error?: string | null;
 }
 
 // -----------------
@@ -22,12 +24,21 @@ export interface TopicState {
 interface GetTopicRequestedAction { type: 'GET_TOPIC_REQUESTED'; }
 interface GetTopicSuccessAction { type: 'GET_TOPIC_SUCCESS'; payload: { topic: GetTopicModel; urlFragment: string; }; }
 interface GetTopicFailedAction { type: 'GET_TOPIC_FAILED'; payload: { error: string; }; }
+interface StatementFormSubmittedAction { type: 'STATEMENT_FORM_SUBMITTED'; }
+interface StatementFormReceivedAction {
+    type: 'STATEMENT_FORM_RECEIVED';
+    payload: { statementListItem: StatementListItemModel; };
+}
+interface StatementFormFailedAction { type: 'STATEMENT_FORM_FAILED'; payload: { error: string | null; }; }
 
 // Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
 // declared type strings (and not any other arbitrary string).
 type KnownAction = GetTopicRequestedAction
     | GetTopicSuccessAction
-    | GetTopicFailedAction;
+    | GetTopicFailedAction
+    | StatementFormSubmittedAction
+    | StatementFormReceivedAction
+    | StatementFormFailedAction;
 
 // ----------------
 // ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
@@ -58,6 +69,48 @@ export const actionCreators = {
             addTask(fetchTask);
         })();
     },
+    submit: (topicUrlFragment: string, statementForm: PostStatementFormModel):
+        AppThunkAction<KnownAction> => (dispatch, getState) => {
+            return (async () => {
+                dispatch({ type: 'STATEMENT_FORM_SUBMITTED' });
+
+                if (statementForm.text) {
+                    const requestOptions: RequestInit = {
+                        body: JSON.stringify(statementForm),
+                        headers: {
+                            'Authorization': 'Bearer ' + getState().login.loggedInUser!.token,
+                            'Content-Type': 'application/json',
+                        },
+                        method: 'POST',
+                    };
+
+                    fetch(`/api/topics/${topicUrlFragment}/statements`, requestOptions)
+                        .then((response: Response) => {
+                            if (response.ok) {
+                                response.json().then((responseModel: StatementListItemModel) => {
+                                    dispatch({
+                                        type: 'STATEMENT_FORM_RECEIVED',
+                                        payload: { statementListItem: responseModel },
+                                    });
+                                });
+                            } else {
+                                response.text().then((text) => {
+                                    dispatch({ type: 'STATEMENT_FORM_FAILED', payload: { error: text } });
+                                });
+                            }
+                        })
+                        .catch((reason: string) => {
+                            dispatch({
+                                type: 'STATEMENT_FORM_FAILED',
+                                payload: { error: reason || 'Posting statement failed' },
+                            });
+                        });
+                } else {
+                    // Don't set an error message, the validation properties will display instead
+                    dispatch({ type: 'STATEMENT_FORM_FAILED', payload: { error: null } });
+                }
+            })();
+        },
 };
 
 // ----------------
@@ -74,6 +127,36 @@ export const reducer: Reducer<TopicState> = (state: TopicState, action: KnownAct
             return { loading: false, topic: action.payload.topic, urlFragment: action.payload.urlFragment };
         case 'GET_TOPIC_FAILED':
             return { loading: false, error: action.payload.error };
+        case 'STATEMENT_FORM_SUBMITTED':
+            return {
+                loading: false,
+                submittingStatement: true,
+                submittedStatement: true,
+                topic: state.topic,
+                urlFragment: state.urlFragment,
+            };
+        case 'STATEMENT_FORM_RECEIVED': {
+            // Slice for immutability
+            const newStatements = state.topic!.statements.slice();
+            newStatements.push(action.payload.statementListItem);
+            const newTopic = { ...state.topic!, statements: newStatements };
+            return {
+                loading: false,
+                submittingStatement: false,
+                submittedStatement: false,
+                topic: newTopic,
+                urlFragment: state.urlFragment,
+            };
+        }
+        case 'STATEMENT_FORM_FAILED':
+            return {
+                loading: false,
+                submittingStatement: false,
+                submittedStatement: true,
+                error: action.payload.error,
+                topic: state.topic,
+                urlFragment: state.urlFragment,
+            };
 
         default:
             // The following line guarantees that every action in the KnownAction union has been covered by a case above
