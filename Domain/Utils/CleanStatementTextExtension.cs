2de1efc832f1212f90e78b17e952dd31845a7cfd
Tim Iles <tim@timiles.com>
2018-05-29 16:42:35 +0100

    Clean Statement Text extension

---
+++ Domain/Utils/CleanStatementTextExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Pobs.Domain.Utils
+{
+    public static class CleanStatementTextExtension
+    {
+        public static string CleanStatementText(this string value)
+        {
+            return value.Trim(' ', '\'', '"', '\t');
+        }
+    }
+}
---
+++ Tests/Unit/CleanStatementTextTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using Pobs.Domain.Utils;
+
+namespace Pobs.Tests.Unit
+{
+    public class CleanStatementTextTests
+    {
+        [Theory]
+        [InlineData("\"Hello there!\"", "Hello there!")]
+        [InlineData("     Hello there!   ", "Hello there!")]
+        [InlineData("\tHello there!\t", "Hello there!")]
+        [InlineData("'Hello there!'", "Hello there!")]
+        [InlineData(" \t \"'Hello there!'\" \t ", "Hello there!")]
+        [InlineData("I said, \"don't remove quotes from inside the text\"!", "I said, \"don't remove quotes from inside the text\"!")]
+        public void Assertions(string value, string expectedValue)
+        {
+            Assert.Equal(expectedValue, value.CleanStatementText());
+        }
+    }
+}
