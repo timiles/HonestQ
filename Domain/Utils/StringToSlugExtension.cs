1d8dad81dd096d51e4263e5dae044def8ce98e5e
Tim Iles <tim@timiles.com>
2018-05-29 12:05:40 +0100

    StringToSlugExtension with tests

---
+++ Domain/Utils/StringToSlugExtension.cs
@@ -0,0 +1,54 @@
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Pobs.Domain.Utils
+{
+    public static class StringToSlugExtension
+    {
+        /*
+         * We will strip invalid characters and delimit words with underscores.
+         * We don't remove stop words - it is probably good practice when we're in 100% control of the slug,
+         * but not necessary, and could instead cause problems for auto generated slugs.
+         * Sources:
+         * - https://moz.com/blog/15-seo-best-practices-for-structuring-urls "#8: ... You don't have to leave them out"
+         * - StackExchange leaves all the words from the question in the slug
+         */
+
+        // White space, full-stop, comma, colon, semicolon, em-dash, en-dash, hyphen, underscore
+        static readonly Regex WordDelimiters = new Regex(@"[\s\.,:;—–\-_]", RegexOptions.Compiled);
+        static readonly Regex InvalidChars = new Regex(@"[^a-z0-9\-_]", RegexOptions.Compiled);
+        static readonly Regex MultipleUnderscores = new Regex(@"_{2,}", RegexOptions.Compiled);
+
+        public static string ToSlug(this string value)
+        {
+            value = value.ToLowerInvariant()
+                .RemoveDiacritics();
+
+            // Replace all word delimiters with underscores
+            value = WordDelimiters.Replace(value, "_");
+
+            // Strip out invalid characters
+            value = InvalidChars.Replace(value, "");
+
+            // Replace multiple underscores with a single underscore
+            value = MultipleUnderscores.Replace(value, "_");
+
+            // Trim underscores from ends
+            return value.Trim('_');
+        }
+
+        private static string RemoveDiacritics(this string value)
+        {
+            // Normalizing splits eg 'é' into 2 chars: 'e' followed by (char)769, an acute NonSpacingMark
+            string normalizedValueFormD = value.Normalize(NormalizationForm.FormD);
+
+            var withoutNonSpacingMarks = normalizedValueFormD
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                .ToArray();
+
+            return new string(withoutNonSpacingMarks).Normalize(NormalizationForm.FormC);
+        }
+    }
+}
---
+++ Tests/Unit/ToSlugTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using Pobs.Domain.Utils;
+
+namespace Pobs.Tests.Unit
+{
+    public class ToSlugTests
+    {
+        [Theory]
+        [InlineData("hello", "hello")]
+        [InlineData("\"Hello there!\"", "hello_there")]
+        [InlineData(" _- Hello there! -_ ", "hello_there")]
+        [InlineData("Hello,there...good-bye -- forever!", "hello_there_good_bye_forever")]
+        [InlineData("Hello there ;-)", "hello_there")]
+        [InlineData("Hello there 😉", "hello_there")]
+        [InlineData("Hello hello!", "hello_hello")]
+        [InlineData("Hello: http://www.example.com", "hello_http_www_example_com")]
+        [InlineData("Let's not remove stop words like 'a' or 'the'; they add meaning",
+            "lets_not_remove_stop_words_like_a_or_the_they_add_meaning")]
+        [InlineData("café, cafetería, cafetière, caïque, calèche, canapé, cañón, cap-à-pie, Champs-Élysées, château",
+            "cafe_cafeteria_cafetiere_caique_caleche_canape_canon_cap_a_pie_champs_elysees_chateau")]
+        public void Assertions(string value, string expectedSlug)
+        {
+            Assert.Equal(expectedSlug, value.ToSlug());
+        }
+    }
+}
