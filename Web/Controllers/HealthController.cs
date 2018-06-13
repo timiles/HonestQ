b8a355f9d6ada3a18f48f2da5a1c226a3835c7de
Tim Iles <tim@timiles.com>
2018-06-13 14:04:50 +0100

    Ping Exceptionless when /api/health?all=true

--- Web/Controllers/HealthController.cs
+++ Web/Controllers/HealthController.cs
@@ -1,12 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
+using Exceptionless;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Net;
+using Microsoft.Extensions.Options;
+using Pobs.Web.Helpers;
 
 namespace Pobs.Web.Controllers
 {
     [Route("api/[controller]")]
     public class HealthController : Controller
     {
-        public IActionResult Index()
+        private readonly string exceptionlessApiKey;
+
+        public HealthController(IOptions<AppSettings> appSettings)
         {
+            this.exceptionlessApiKey = appSettings.Value.ExceptionlessApiKey;
+        }
+
+        public IActionResult Index(bool all)
+        {
+            ExceptionlessClient exceptionlessClient = null;
+            var errors = new Dictionary<string, Exception>();
+            if (all)
+            {
+                if (this.exceptionlessApiKey != null)
+                {
+                    exceptionlessClient = new ExceptionlessClient(this.exceptionlessApiKey);
+                    try
+                    {
+                        // Test our connection to exceptionless (if it was initialized)
+                        exceptionlessClient.SubmitLog("Health check");
+                        throw new Exception();
+                    }
+                    catch (Exception ex)
+                    {
+                        errors.Add("Could not connect to Exceptionless", ex);
+                    }
+                }
+            }
+
+            if (errors.Any())
+            {
+                if (exceptionlessClient != null)
+                {
+                    foreach (var error in errors)
+                    {
+                        try
+                        {
+                            exceptionlessClient.SubmitException(error.Value);
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                return Content("ERROR: " + string.Join("; ", errors.Keys));
+            }
             return Content("OK");
         }
     }
