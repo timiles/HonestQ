6c92dacde3f26cf8275e474608e9cca37574df43
Tim Iles <tim@timiles.com>
2019-01-15 16:33:34 +0000

    Extract Spinner into its own component

--- Web/ClientApp/components/shared/SubmitButton.tsx
+++ Web/ClientApp/components/shared/SubmitButton.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Spinner from './Spinner';
 
 interface SubmitButtonProps {
     submitting?: boolean;
@@ -9,9 +10,9 @@ export default class SubmitButton extends React.Component<SubmitButtonProps, {}>
     public render() {
         const { text, submitting } = this.props;
         return (
-            <button className="btn btn-primary" disabled={submitting}>{text || 'Submit'}
-                {/* tslint:disable-next-line:max-line-length */}
-                {submitting && <img className="ml-2" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />}
+            <button className="btn btn-primary" disabled={submitting}>
+                {text || 'Submit'}
+                {submitting && <Spinner />}
             </button>
         );
     }
