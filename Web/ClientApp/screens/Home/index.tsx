b1c38ce5338564307cc810ece8b7877677f8cae2
Tim Iles <tim@timiles.com>
2019-03-20 12:06:43 +0000

    Increase size of QuestionSearchControl on md devices

--- Web/ClientApp/screens/Home/index.tsx
+++ Web/ClientApp/screens/Home/index.tsx
@@ -19,7 +19,7 @@ class Index extends React.Component<Props> {
                         <div className="container">
                             <WelcomeMessage />
                             <div className="row text-center">
-                                <div className="col-md-6 offset-md-3">
+                                <div className="col-lg-6 offset-lg-3">
                                     <QuestionSearchControl />
                                     <p className="welcome-message or-browse-by-tag">Or browse by Tags</p>
                                     <TagsList buttonSize="md" numberOfTagsToShow={11} />
