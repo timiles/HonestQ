2b1d73fa60b14523dc9152c1c925177e3a190bd1
Tim Iles <tim@timiles.com>
2019-08-28 19:11:04 +0100

    Tweaks to email template

--- server/Comms/EmailVerificationEmailMessage.cs
+++ server/Comms/EmailVerificationEmailMessage.cs
@@ -7,20 +7,20 @@ namespace Pobs.Comms
 
         internal EmailVerificationEmailMessage(string username, string verifyUrl)
         {
-            _username = username;
-            _verifyUrl = verifyUrl;
+        _username = username;
+        _verifyUrl = verifyUrl;
         }
 
         public string From => "verify@honestq.com";
 
-        public string Subject => $"Verify your HonestQ.com account, {_username}.";
+        public string Subject => $"Verify your HonestQ account, {_username}.";
 
         public string BodyHtml => $@"
-            <h1>Verify your HonestQ.com account, {_username}.</h1>
-            <p>Click the link below to verify your email address:</p>
-            <p><a href=""{_verifyUrl}"">Verify</a></p>
-            <p>HonestQ.com</p>
-            <small>Didn't sign up to HonestQ.com? Simply ignore this email and you won't hear from us again.</small>
-            ";
+                <h2>Hi {_username},</h2>
+                <p>Click the link below to verify your email address:</p>
+                <p><a href=""{_verifyUrl}"">Verify</a></p>
+                <p>HonestQ</p>
+                <small>Didn't sign up to HonestQ? Simply ignore this email and you won't hear from us again.</small>
+                ";
     }
 }
