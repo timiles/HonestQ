c488b07b607c2c0f15733cd7a49c2b35190dae8c
Tim Iles <tim@timiles.com>
2019-09-13 21:54:19 +0100

    QuestionCard as a button

--- mobile/HonestQ/src/components/QuestionCard.tsx
+++ mobile/HonestQ/src/components/QuestionCard.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-import { HQNavigationButton, HQText } from '../hq-components';
+import { TouchableOpacity, View } from 'react-native';
+import { HQLabel, HQText } from '../hq-components';
 import hqStyles from '../hq-styles';
 import NavigationService from '../NavigationService';
 import { QuestionNavigationProps } from '../screens/QuestionScreen';
 import { QuestionListItemModel } from '../server-models';
 import { getItemCountText } from '../utils/string-utils';
-import TopCircleIconCard from './TopCircleIconCard';
+import IconCard from './IconCard';
 
 interface Props {
   question: QuestionListItemModel;
@@ -19,17 +19,16 @@ export default class QuestionCard extends React.Component<Props> {
     const { id, text, answersCount } = question;
 
     return (
-      <TopCircleIconCard type="Q">
-        <View style={hqStyles.mv1}>
+      <TouchableOpacity
+        onPress={() => this.navigateToQuestion(id)}
+      >
+        <IconCard type="Q">
           <View style={hqStyles.mb1}>
             <HQText>{text}</HQText>
           </View>
-          <HQNavigationButton
-            title={getItemCountText('answer', answersCount)}
-            onPress={() => this.navigateToQuestion(id)}
-          />
-        </View>
-      </TopCircleIconCard>
+          <HQLabel>{getItemCountText('answer', answersCount)}</HQLabel>
+        </IconCard>
+      </TouchableOpacity>
     );
   }
 
