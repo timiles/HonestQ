ad5722132f682089691a63773b049cbf8f05e053
Tim Iles <tim@timiles.com>
2019-09-15 14:03:18 +0100

    Fix indentation of model-utils

--- mobile/HonestQ/src/utils/model-utils.ts
+++ mobile/HonestQ/src/utils/model-utils.ts
@@ -1,39 +1,39 @@
 import { CommentModel } from './../server-models';
 
 export function countNestedComments(comments: CommentModel[]): number {
-    let total = comments.length;
-    for (const comment of comments) {
-        // Also add any child comments
-        if (comment.comments.length > 0) {
-            total += countNestedComments(comment.comments);
-        }
+  let total = comments.length;
+  for (const comment of comments) {
+    // Also add any child comments
+    if (comment.comments.length > 0) {
+      total += countNestedComments(comment.comments);
     }
-    return total;
+  }
+  return total;
 }
 
 export function getCommentScores(comments: CommentModel[]): [number, number] {
-    let agreeCount = 0;
-    let disagreeCount = 0;
-    for (const comment of comments) {
-        if (comment.agreementRating === 'Agree') {
-            agreeCount++;
-        }
-        if (comment.agreementRating === 'Disagree') {
-            disagreeCount++;
-        }
+  let agreeCount = 0;
+  let disagreeCount = 0;
+  for (const comment of comments) {
+    if (comment.agreementRating === 'Agree') {
+      agreeCount++;
     }
-    return [agreeCount, disagreeCount];
+    if (comment.agreementRating === 'Disagree') {
+      disagreeCount++;
+    }
+  }
+  return [agreeCount, disagreeCount];
 }
 
 export function findComment(comments: CommentModel[], commentId: number): CommentModel | null {
-    for (const comment of comments) {
-        if (comment.id === commentId) {
-            return comment;
-        }
-        const childComment = findComment(comment.comments, commentId);
-        if (childComment) {
-            return childComment;
-        }
+  for (const comment of comments) {
+    if (comment.id === commentId) {
+      return comment;
     }
-    return null;
+    const childComment = findComment(comment.comments, commentId);
+    if (childComment) {
+      return childComment;
+    }
+  }
+  return null;
 }
